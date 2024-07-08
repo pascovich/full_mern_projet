db6b76bc68fc02825a8b76f695a3e593cc1350f9
pascovich <kaluzipascovich@gmail.com>
2024-07-08 14:08:41 +0200

    like unlike

--- controllers/postController.js
+++ controllers/postController.js
@@ -65,3 +65,75 @@ export const deletePost = async (req, res) => {
     res.status(500).send({ message: err.message });
   }
 };
+
+export const likePost = async (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(404).send({ message: "Unknown id" });
+  try {
+    //add to follower list
+    await postModel
+      .findByIdAndUpdate(
+        req.params.id,
+        { $addToSet: { likers: req.body.idPers } },
+        { new: true }
+      )
+      .then((docs) => {
+        res.send({ message: "post liked successfully", docs });
+      })
+      .catch((err) => {
+        return res.status(500).send({ message: err });
+      });
+
+    //add to following list
+    await userModel
+      .findByIdAndUpdate(
+        req.body.idPers,
+        { $addToSet: { likes: req.params.id } },
+        { new: true }
+      )
+      .then((docs) => {
+        // res.send({ message: "user following successfully", docs });
+      })
+      .catch((err) => {
+        return res.status(500).send({ message: err });
+      });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+export const unlikePost = async (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(404).send({ message: "Unknown id" });
+  try {
+    //add to follower list
+    await postModel
+      .findByIdAndUpdate(
+        req.params.id,
+        { $pull: { likers: req.body.id } },
+        { new: true }
+      )
+      .then((docs) => {
+        res.send({ message: "post unliked successfully", docs });
+      })
+      .catch((err) => {
+        return res.status(500).send({ message: err });
+      });
+
+    //add to following list
+    await userModel
+      .findByIdAndUpdate(
+        req.body.id,
+        { $pull: { likes: req.params.id } },
+        { new: true }
+      )
+      .then((docs) => {
+        // res.send({ message: "user following successfully", docs });
+      })
+      .catch((err) => {
+        return res.status(500).send({ message: err });
+      });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,6 +4,8 @@ import {
   createPost,
   updatePost,
   deletePost,
+  likePost,
+  unlikePost,
 } from "../controllers/postController.js";
 
 const router = express.Router();
@@ -12,6 +14,8 @@ router.get("/", readPost);
 router.post("/", createPost);
 router.put("/:id", updatePost);
 router.delete("/:id", deletePost);
+router.patch("/like-post/:id", likePost);
+router.patch("/unlike-post/:id", unlikePost);
 
 // export router
 export default router;
